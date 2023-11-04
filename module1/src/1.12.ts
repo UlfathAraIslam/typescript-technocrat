7e303127dc07cc7c2ee2a37f50f6721c9094c981
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-11-05 07:11:33 +0900

    type vs interface

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,42 +1,38 @@
 {
-    // 
-    // nullable types
-
-    const searchName= (value: string | null) => {
-        if(value){
-            console.log('Searching');
-        } else {
-            console.log('There is nothing to search');
-        }
+    // nullable types / unknown types
+  
+    const searchName = (value: string | null) => {
+      if (value) {
+        console.log("Searching");
+      } else {
+        console.log("There is nothing to search");
+      }
     };
-
     searchName(null);
-
-    // unknown types f(typeof)
-
-    const getSpeedInMeterPerSecond=(value:unknown)=> {
-        if(typeof value === 'number'){
-            const convertedSpeed = (value*1000) /3600;
-            console.log(`The Speed is ${convertedSpeed} ms^-1`);
-        }
-        else if(typeof value === 'string'){
-            const [result,unit] = value.split(" ");
-            const convertedSpeed = (parseFloat(result)*1000) /3600;
-            console.log(`The Speed is ${convertedSpeed} ms^-1`);
-        }
-        else{
-            console.log('wrong input');
-        }
+  
+    // unknown  typeof
+  
+    const getSpeedInMeterPerSecond = (value: unknown) => {
+      if (typeof value === "number") {
+        const convertedSpeed = (value * 1000) / 3600;
+        console.log(`The speed is ${convertedSpeed} ms^-1`);
+      } else if (typeof value === "string") {
+        const [result, unit] = value.split(" ");
+        const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+        console.log(`The speed is ${convertedSpeed} ms^-1`);
+      } else {
+        console.log("wrong input");
+      }
     };
     getSpeedInMeterPerSecond(null);
-
-    // never
-
-    function throwError(msg: string): never {
-        throw new Error(msg);
-    }
-
-    throwError('mistakenly got error')
-    // 
-
-}
+  
+    //never
+  
+    const throwError = (msg: string): never => {
+      throw new Error(msg);
+    };
+  
+    throwError("mushkil se error hogaya");
+  
+    //
+  }
